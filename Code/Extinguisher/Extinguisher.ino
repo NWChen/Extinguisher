5c931d585cbcb651e9507a1bd36c4e8f30ce77f1
NWChen <neilwchen@gmail.com>
2016-04-09 23:31:36 -0400

    Gradual turning function

--- Code/Extinguisher/Extinguisher.ino
+++ Code/Extinguisher/Extinguisher.ino
@@ -146,11 +146,13 @@ void turnRight(int speed) {
 }
 
 void halfTurnLeft(int speed) {
+  motor(LEFT_MOTOR, FORWARD, 150);
   motor(RIGHT_MOTOR, BACKWARD, speed);
 }
 
 void halfTurnRight(int speed) {
   motor(LEFT_MOTOR, BACKWARD, speed);
+  motor(RIGHT_MOTOR, FORWARD, 150);
 }
 
 void releaseAllMotors() {
